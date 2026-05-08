9ea3c0c4ddec8bb1b399cb8bb6bf9e64203f7d23
Gurdeep Singh (Guru) <oyeaussie@gmail.com>
2026-05-08 23:54:53 +0530

    Initial Commit

---
+++ Widgets.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Apps\Tms\Components\System\Users\Profile;
+
+use System\Base\Providers\ModulesServiceProvider\Modules\Components\ComponentsWidgets;
+
+class Widgets extends ComponentsWidgets
+{}
